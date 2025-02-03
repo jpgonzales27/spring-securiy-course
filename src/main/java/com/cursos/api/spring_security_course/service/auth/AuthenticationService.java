e73d9e72c680c96f456f0b794a647dc95fcc6b88
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-02-02 21:14:46 -0400

    creando controller para registrar un new user

---
+++ src/main/java/com/cursos/api/spring_security_course/service/auth/AuthenticationService.java
@@ -0,0 +1,15 @@
+package com.cursos.api.spring_security_course.service.auth;
+
+import com.cursos.api.spring_security_course.dto.RegisteredUser;
+import com.cursos.api.spring_security_course.dto.SaveUser;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    public RegisteredUser registerOneCustomer(SaveUser newUser) {
+
+
+        return null;
+    }
+}
