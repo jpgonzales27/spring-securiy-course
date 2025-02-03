3a38f14c0b0fc0a5bfca6926771edfca587aa1a3
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-02-03 08:53:25 -0400

    creado servicio JWT para generar token y usando en el servicio para registrar un customer

--- src/main/java/com/cursos/api/spring_security_course/service/auth/AuthenticationService.java
+++ src/main/java/com/cursos/api/spring_security_course/service/auth/AuthenticationService.java
@@ -2,14 +2,45 @@ package com.cursos.api.spring_security_course.service.auth;
 
 import com.cursos.api.spring_security_course.dto.RegisteredUser;
 import com.cursos.api.spring_security_course.dto.SaveUser;
+import com.cursos.api.spring_security_course.persistence.entity.User;
+import com.cursos.api.spring_security_course.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class AuthenticationService {
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtService jwtService;
+
     public RegisteredUser registerOneCustomer(SaveUser newUser) {
+        User user = userService.registerOneCustomer(newUser);
 
+        RegisteredUser userDto = new RegisteredUser();
+        userDto.setId(user.getId());
+        userDto.setName(user.getName());
+        userDto.setUsername(user.getUsername());
+        userDto.setRole(user.getRole().name());
 
-        return null;
+        String jwt = jwtService.generateToken(user,generateExtraClaims(user));
+        userDto.setJwt(jwt);
+
+        return userDto;
     }
+
+    private Map<String, Object> generateExtraClaims(User user) {
+        Map<String, Object> extraClaims = new HashMap<>();
+        extraClaims.put("name",user.getName());
+        extraClaims.put("role",user.getRole().name());
+        extraClaims.put("authorities",user.getAuthorities());
+
+        return extraClaims;
+    }
+
 }
